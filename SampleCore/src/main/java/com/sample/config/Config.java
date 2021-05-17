7b6ed00067e45ba0bbf623b82affa65b3942301a
furkansavsatli <furkansavsatli@gmail.com>
2021-05-17 18:14:35 +0300

    Update Config.java

--- SampleCore/src/main/java/com/sample/config/Config.java
+++ SampleCore/src/main/java/com/sample/config/Config.java
@@ -9,6 +9,6 @@ import org.springframework.context.annotation.Configuration;
 public class Config {
 
     @Autowired
-    public SampleService licenceService;
+    public SampleService sampleService;
 
 }
