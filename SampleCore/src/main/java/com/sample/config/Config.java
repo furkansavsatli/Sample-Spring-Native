0a7bfa318d84dfcd0bb6b464ca3d208393148ba9
furkansavsatli <furkansavsatli@gmail.com>
2021-05-17 18:12:40 +0300

    Add files via upload
    initial

---
+++ SampleCore/src/main/java/com/sample/config/Config.java
@@ -0,0 +1,14 @@
+package com.sample.config;
+
+
+import com.sample.service.SampleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Autowired
+    public SampleService licenceService;
+
+}
---
+++ SampleLibrary/src/main/java/com/sample/service/SampleService.java
@@ -0,0 +1,12 @@
+package com.sample.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public SampleService() {
+        System.out.println("i'am groot");
+    }
+}
