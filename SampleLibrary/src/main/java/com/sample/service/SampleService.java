80ddee01bf76a22ca1b29f5ee29bbed64c565b53
fsavsatli <fsavsatli@havelsan.com.tr>
2021-08-04 17:04:12 +0300

    Resource test update

--- SampleLibrary/src/main/java/com/sample/service/SampleService.java
+++ SampleLibrary/src/main/java/com/sample/service/SampleService.java
@@ -1,12 +1,27 @@
 package com.sample.service;
 
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 @Service
 public class SampleService {
 
-    public SampleService() {
-        System.out.println("i'am groot");
+    public SampleService() throws IOException {
+        System.out.println("i'am groot 1");
+        InputStream resource = new ClassPathResource(
+                "files.txt").getInputStream();
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(resource))) {
+            String msg = reader.lines()
+                    .collect(Collectors.joining("\n"));
+            System.out.println(msg);
+        }
     }
 }
